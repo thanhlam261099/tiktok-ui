f91dd130193164e4d49e2e475e5b1299d3f314f1
thanhlam261099 <thanhlam99kt@gmail.com>
2022-08-01 22:55:54 +0700

    Dựng base dự án

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { HeaderOnly } from '~/components/Layout'
+
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/Following', component: Following },
+    { path: '/Profile', component: Profile },
+    { path: '/Upload', component: Upload, layout: HeaderOnly },
+    { path: '/Search', component: Search, layout: null }
+]
+
+const privateRoutes = [
+    
+]
+
+export { publicRoutes, privateRoutes }
